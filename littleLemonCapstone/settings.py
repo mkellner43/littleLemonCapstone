8c3732e3a820737589b34d2129c899473ff3017e
mkellner43 <matthew.kellner@outlook.com>
2023-11-27 19:31:54 -0600

    removed unused app

--- littleLemonCapstone/settings.py
+++ littleLemonCapstone/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "restaurant",
-    "reservation",
     "rest_framework",
     "rest_framework.authtoken",
     "djoser",
